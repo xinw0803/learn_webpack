b5fbb941bc915badde1bea565234c691bf8d33cb
xin.wang <xin.wang@alo7.com>
2019-09-26 11:04:54 +0800

    feat: 修改提交前代码格式化配置

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,43 @@ import * as React from "react";
 import Container from "./Container";
 export interface IProps {}
 
+const withMouse = (Component: any) => {
+  return class extends React.Component {
+    state = {
+      x: 0,
+      y: 0
+    };
+
+    private handleMouseMove = (e: React.MouseEvent) => {
+      this.setState({
+        x: e.clientX,
+        y: e.clientY
+      });
+    };
+
+    render() {
+      return (
+        <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
+          <Component {...this.props} mouse={this.state} />
+        </div>
+      );
+    }
+  };
+};
+
+const Content = (props: any) => {
+  const { x, y } = props.mouse;
+  return (
+    <div>
+      <h1>
+        鼠标坐标({x}, {y})
+      </h1>
+    </div>
+  );
+};
+
+const ComponentWithMouse = withMouse(Content);
+
 export default class App extends React.Component<IProps, {}> {
   // constructor(props: IProps){
   //   super(props);
@@ -20,6 +57,7 @@ export default class App extends React.Component<IProps, {}> {
     return (
       <Container>
         <div>内容</div>
+        <ComponentWithMouse />
       </Container>
     );
   }
